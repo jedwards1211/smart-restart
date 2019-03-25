e8e10dc683b9e78c77905050e9782ea9ef3395d2
Andy Edwards <jedwards@fastmail.com>
2019-03-25 12:29:30 -0500

    feat: support rerequire option

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import chalk from 'chalk'
 import path from 'path'
 import {spawn} from 'child_process'
-import chokidar from 'chokidar'
 
 const debug = require('debug')('smart-restart:supervisor')
 
 module.exports = function launch(ops) {
   let lastErr = ''
   let child
-  let watcher
   const options = {
     includeModules: false,
     ignore: /(\/\.|~$)/,
@@ -17,10 +15,10 @@ module.exports = function launch(ops) {
     commandOptions: [],
     spawnOptions: {},
     args: [],
+    rerequire: {},
     ...ops,
   }
   if (!options.main) throw new Error('missing main')
-  const initial = path.resolve(options.main)
 
   process.on('exit', () => child && child.kill())
 
@@ -29,19 +27,6 @@ module.exports = function launch(ops) {
       debug('killing child...')
       child.kill()
     }
-    if (watcher) watcher.close()
-
-    watcher = chokidar.watch(initial, {
-      ignored: options.ignore,
-      ignoreInitial: false,
-      usePolling: options.usePolling,
-      interval: options.interval || 100,
-      binaryInterval: options.binaryInterval || 300
-    })
-    watcher.on("change", function(file) {
-      console.log(chalk.bold.red("[smart-restart]"), "File", path.relative(process.cwd(), file), "has changed, reloading.")
-      respawn()
-    })
 
     const args = [
       options.command,
@@ -62,6 +47,11 @@ module.exports = function launch(ops) {
     debug('spawned child pid: ', child.pid)
     child.on('message', message => {
       debug('message received')
+      if (message.respawn) {
+        console.log(chalk.bold.red("[smart-restart]"), "File", path.relative(process.cwd(), message.file), "has changed, reloading.")
+        respawn()
+        return
+      }
       if (message.status === 'ready') {
         debug('sending message:', options)
         child.send(options, error => {
@@ -77,9 +67,6 @@ module.exports = function launch(ops) {
           console.log(chalk.bold.red("[smart-restart]"), "further repeats of this error will be suppressed...")
           respawn()
         }
-      } else if (message.file) {
-        debug('watching file: ', path.resolve(message.file))
-        watcher.add(path.resolve(message.file))
       }
     })
   }
--- src/launcher.js
+++ src/launcher.js
@@ -1,19 +1,45 @@
 import path from 'path'
+import chokidar from 'chokidar'
+import chalk from 'chalk'
 const debug = require('debug')('smart-restart:launcher')
 
 const natives = process.binding('natives')
 
 process.on('message', options => {
   debug('message received')
+
+  const rerequire = options.rerequire || {}
+  const origRerequire = {...rerequire}
+
+  const initial = path.resolve(options.main)
+  const watcher = chokidar.watch(initial, {
+    ignored: options.ignore,
+    ignoreInitial: false,
+    usePolling: options.usePolling,
+    interval: options.interval || 100,
+    binaryInterval: options.binaryInterval || 300
+  })
+  watcher.on("change", function(file) {
+    if (rerequire[file]) {
+      console.log(chalk.bold.red("[smart-restart]"), "File", path.relative(process.cwd(), file), "has changed, clearing require cache")
+      for (let key in rerequire) delete require.cache[key]
+      return
+    }
+    watcher.close()
+    process.send({respawn: true, file})
+  })
+
   const main = path.resolve(options.main)
   const module = require("module")
   const _load_orig = module._load
   module._load = function (name, parent, isMain) {
     const file = module._resolveFilename(name, parent)
+    if (!origRerequire.hasOwnProperty(file)) {
+      rerequire[file] = rerequire[file] || rerequire[parent.id] || false
+    }
     if (options.includeModules || file.indexOf('node_modules') < 0) {
       if (!natives[file] && file !== main) {
-        debug('sending message: ', {file})
-        process.send({file})
+        watcher.add(file)
       }
       else debug('ignoring module: ', name)
     } else {
